38d46795730d75aa3139bdd731dd69000936fd22
fabiosantander2007-spec <kriziathais2007@gmail.com>
2026-04-09 14:10:44 -0500

    archivos

---
+++ backend/src/test/java/com/senati/gotagotA/GotagotAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senati.gotagotA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotagotAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
