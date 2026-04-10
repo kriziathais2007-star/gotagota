29882cadd03664570ba16a0cd72b38513f428589
kriziathais2007-star <kriziathais2007@gmail.com>
2026-04-10 13:36:55 -0500

    o

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,11 +1,11 @@
-// EVENTOS DE JAVASCRIPT (CLICK, CARGAR, KEY)
+// EVENTOS EN JAVASCRIPT (CLICK, CARGAR, KEY,)
 document.addEventListener("DOMContentLoaded", () => {
     fetch('http://localhost:8080/api/clientes')
         .then(response => response.json())
         .then(data => {
             console.log('Datos:', data);
             const elemento = document.getElementById("tabla-cliente")
-            data.forEach(c => elemento.innerHTML += `<tr><td>${c.id}</td><td>${c.nombre}</td></tr>`);
+            elemento.innerHTML = JSON.stringify(data);
             console.log(elemento)
         })
 });
