66dfa28f7e433ca0b90584241f6d9b7d4ef13bc2
kriziathais2007-star <kriziathais2007@gmail.com>
2026-04-17 15:27:06 -0500

    cambio

--- frontend/js/sidebar.js
+++ frontend/js/sidebar.js
@@ -3,18 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.querySelector(".hamburger");
     const sidebar   = document.querySelector(".sidebar");
     const overlay   = document.querySelector(".overlay");
-
     function openSidebar() {
         sidebar.classList.add("open");
         overlay.classList.add("show");
     }
-
     function closeSidebar() {
         sidebar.classList.remove("open");
         overlay.classList.remove("show");
     }
-
     hamburger.addEventListener("click", openSidebar);
     overlay.addEventListener("click", closeSidebar);
-
-    });
+});
